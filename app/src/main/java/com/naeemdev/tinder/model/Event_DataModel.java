31a0de2e018cd6b231d19b0b4371287a22c9723f
= <=>
2023-04-05 13:17:03 -0300

    Design

---
+++ app/src/main/java/com/naeemdev/tinder/model/Event_DataModel.java
@@ -0,0 +1,10 @@
+package com.naeemdev.tinder.model;
+
+public class Event_DataModel {
+
+    public final String message;
+
+    public Event_DataModel(String message) {
+        this.message = message;
+    }
+}
